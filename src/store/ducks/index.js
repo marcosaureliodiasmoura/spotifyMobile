6e9784f041b0e529608ae0b540d019b2aa3e0b25
MarcosMoura <marcos_moura@live.com>
2019-07-02 08:56:47 -0300

    Redux Sauce e Immutable

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,19 +1,7 @@
-// Responsável pelos reducers e actions
-// Responsável por fazer o CombineReducers
-
 import { combineReducers } from 'redux';
-import { reducer as auth } from './auth';
-import { reducer as projects } from './projects';
-// import users from './users';
-// import donations from './donations';
-// import projectsDetails from './projectsDetails';
-// import error from './error';
+
+import { reducer as podcasts } from './podcasts';
 
 export default combineReducers({
-  auth,
-  projects,
-  // users,
-  // donations,
-  // projectsDetails,
-  // error,
+  podcasts,
 });
---
+++ src/store/ducks/podcasts.js
@@ -0,0 +1,22 @@
+import { createReducer, createActions } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+
+// Action Types e Creators
+const { Types, Creators } = createActions({
+  loadRequest: null,
+  loadSuccess: ['data'],
+  loadFailure: null,
+});
+
+export const PodcastsTypes = Types;
+export default Creators;
+
+// Initial State
+export const INITIAL_STATE = Immutable({
+  data: [],
+});
+
+// Reducer (ActionTypes na Esquerda e o que vai acontecer com o nosso Estado na direita)
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.LOAD_SUCCESS]: (state, { data }) => state.merge({ data }),
+});
